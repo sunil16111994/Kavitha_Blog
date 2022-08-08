2ad2f23f8b8f6f75f3b3bfb3a8848bc440599b71
sunil <sunil.sm56@gmail.com>
2022-08-08 10:19:44 +0530

    "first commit"

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ from flask_login import UserMixin, login_user, LoginManager, login_required, cur
 from forms import LoginForm, RegisterForm, CreatePostForm, CommentForm
 from flask_gravatar import Gravatar
 
-
+#
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
 ckeditor = CKEditor(app)
